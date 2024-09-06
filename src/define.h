07b96c2dab44c93f0e9aed74feb11fd8bc5301b6
molingyu <z1522716486@hotmail.com>
2024-09-06 22:43:58 +0800

    feat: add define.h
    Language Code

---
+++ src/define.h
@@ -0,0 +1,360 @@
+//        GNU LESSER GENERAL PUBLIC LICENSE
+//                        Version 3, 29 June 2007
+//
+//  Copyright (C) 2024 I18n-format contributors.
+//  Everyone is permitted to copy and distribute verbatim copies
+//  of this license document, but changing it is not allowed.
+//
+//
+//   This version of the GNU Lesser General Public License incorporates
+// the terms and conditions of version 3 of the GNU General Public
+// License, supplemented by the additional permissions listed below.
+//
+//   0. Additional Definitions.
+//
+//   As used herein, "this License" refers to version 3 of the GNU Lesser
+// General Public License, and the "GNU GPL" refers to version 3 of the GNU
+// General Public License.
+//
+//   "The Library" refers to a covered work governed by this License,
+// other than an Application or a Combined Work as defined below.
+//
+//   An "Application" is any work that makes use of an interface provided
+// by the Library, but which is not otherwise based on the Library.
+// Defining a subclass of a class defined by the Library is deemed a mode
+// of using an interface provided by the Library.
+//
+//   A "Combined Work" is a work produced by combining or linking an
+// Application with the Library.  The particular version of the Library
+// with which the Combined Work was made is also called the "Linked
+// Version".
+//
+//   The "Minimal Corresponding Source" for a Combined Work means the
+// Corresponding Source for the Combined Work, excluding any source code
+// for portions of the Combined Work that, considered in isolation, are
+// based on the Application, and not on the Linked Version.
+//
+//   The "Corresponding Application Code" for a Combined Work means the
+// object code and/or source code for the Application, including any data
+// and utility programs needed for reproducing the Combined Work from the
+// Application, but excluding the System Libraries of the Combined Work.
+//
+//   1. Exception to Section 3 of the GNU GPL.
+//
+//   You may convey a covered work under sections 3 and 4 of this License
+// without being bound by section 3 of the GNU GPL.
+//
+//   2. Conveying Modified Versions.
+//
+//   If you modify a copy of the Library, and, in your modifications, a
+// facility refers to a function or data to be supplied by an Application
+// that uses the facility (other than as an argument passed when the
+// facility is invoked), then you may convey a copy of the modified
+// version:
+//
+//    a) under this License, provided that you make a good faith effort to
+//    ensure that, in the event an Application does not supply the
+//    function or data, the facility still operates, and performs
+//    whatever part of its purpose remains meaningful, or
+//
+//    b) under the GNU GPL, with none of the additional permissions of
+//    this License applicable to that copy.
+//
+//   3. Object Code Incorporating Material from Library Header Files.
+//
+//   The object code form of an Application may incorporate material from
+// a header file that is part of the Library.  You may convey such object
+// code under terms of your choice, provided that, if the incorporated
+// material is not limited to numerical parameters, data structure
+// layouts and accessors, or small macros, inline functions and templates
+// (ten or fewer lines in length), you do both of the following:
+//
+//    a) Give prominent notice with each copy of the object code that the
+//    Library is used in it and that the Library and its use are
+//    covered by this License.
+//
+//    b) Accompany the object code with a copy of the GNU GPL and this license
+//    document.
+//
+//   4. Combined Works.
+//
+//   You may convey a Combined Work under terms of your choice that,
+// taken together, effectively do not restrict modification of the
+// portions of the Library contained in the Combined Work and reverse
+// engineering for debugging such modifications, if you also do each of
+// the following:
+//
+//    a) Give prominent notice with each copy of the Combined Work that
+//    the Library is used in it and that the Library and its use are
+//    covered by this License.
+//
+//    b) Accompany the Combined Work with a copy of the GNU GPL and this license
+//    document.
+//
+//    c) For a Combined Work that displays copyright notices during
+//    execution, include the copyright notice for the Library among
+//    these notices, as well as a reference directing the user to the
+//    copies of the GNU GPL and this license document.
+//
+//    d) Do one of the following:
+//
+//        0) Convey the Minimal Corresponding Source under the terms of this
+//        License, and the Corresponding Application Code in a form
+//        suitable for, and under terms that permit, the user to
+//        recombine or relink the Application with a modified version of
+//        the Linked Version to produce a modified Combined Work, in the
+//        manner specified by section 6 of the GNU GPL for conveying
+//        Corresponding Source.
+//
+//        1) Use a suitable shared library mechanism for linking with the
+//        Library.  A suitable mechanism is one that (a) uses at run time
+//        a copy of the Library already present on the user's computer
+//        system, and (b) will operate properly with a modified version
+//        of the Library that is interface-compatible with the Linked
+//        Version.
+//
+//    e) Provide Installation Information, but only if you would otherwise
+//    be required to provide such information under section 6 of the
+//    GNU GPL, and only to the extent that such information is
+//    necessary to install and execute a modified version of the
+//    Combined Work produced by recombining or relinking the
+//    Application with a modified version of the Linked Version. (If
+//    you use option 4d0, the Installation Information must accompany
+//    the Minimal Corresponding Source and Corresponding Application
+//    Code. If you use option 4d1, you must provide the Installation
+//    Information in the manner specified by section 6 of the GNU GPL
+//    for conveying Corresponding Source.)
+//
+//   5. Combined Libraries.
+//
+//   You may place library facilities that are a work based on the
+// Library side by side in a single library together with other library
+// facilities that are not Applications and are not covered by this
+// License, and convey such a combined library under terms of your
+// choice, if you do both of the following:
+//
+//    a) Accompany the combined library with a copy of the same work based
+//    on the Library, uncombined with any other library facilities,
+//    conveyed under the terms of this License.
+//
+//    b) Give prominent notice with the combined library that part of it
+//    is a work based on the Library, and explaining where to find the
+//    accompanying uncombined form of the same work.
+//
+//   6. Revised Versions of the GNU Lesser General Public License.
+//
+//   The Free Software Foundation may publish revised and/or new versions
+// of the GNU Lesser General Public License from time to time. Such new
+// versions will be similar in spirit to the present version, but may
+// differ in detail to address new problems or concerns.
+//
+//   Each version is given a distinguishing version number. If the
+// Library as you received it specifies that a certain numbered version
+// of the GNU Lesser General Public License "or any later version"
+// applies to it, you have the option of following the terms and
+// conditions either of that published version or of any later version
+// published by the Free Software Foundation. If the Library as you
+// received it does not specify a version number of the GNU Lesser
+// General Public License, you may choose any version of the GNU Lesser
+// General Public License ever published by the Free Software Foundation.
+//
+//   If the Library as you received it specifies that a proxy can decide
+// whether future versions of the GNU Lesser General Public License shall
+// apply, that proxy's public statement of acceptance of any version is
+// permanent authorization for you to choose that version for the
+// Library.
+
+#pragma once
+
+extern char* fallback_lang;
+
+/* ISO 639-1 Language Code */
+
+#define LANG_AA "aa"        // Afar
+#define LANG_AB "ab"        // Abkhazian
+#define LANG_AE "ae"        // Avestan
+#define LANG_AF "af"        // Afrikaans
+#define LANG_AK "ak"        // Akan
+#define LANG_AM "am"        // Amharic
+#define LANG_AN "an"        // Aragonese
+#define LANG_AR "ar"        // Arabic
+#define LANG_AS "as"        // Assamese
+#define LANG_AV "av"        // Avaric
+#define LANG_AY "ay"        // Aymara
+#define LANG_AZ "az"        // Azerbaijani
+#define LANG_BA "ba"        // Bashkir
+#define LANG_BE "be"        // Belarusian
+#define LANG_BG "bg"        // Bulgarian
+#define LANG_BH "bh"        // Bihari
+#define LANG_BI "bi"        // Bislama
+#define LANG_BM "bm"        // Bambara
+#define LANG_BN "bn"        // Bengali
+#define LANG_BO "bo"        // Tibetan
+#define LANG_BR "br"        // Breton
+#define LANG_BS "bs"        // Bosnian
+#define LANG_CA "ca"        // Catalan
+#define LANG_CE "ce"        // Chechen
+#define LANG_CH "ch"        // Chamorro
+#define LANG_CO "co"        // Corsican
+#define LANG_CR "cr"        // Cree
+#define LANG_CS "cs"        // Czech
+#define LANG_CU "cu"        // Church Slavic
+#define LANG_CV "cv"        // Chuvash
+#define LANG_CY "cy"        // Welsh
+#define LANG_DA "da"        // Danish
+#define LANG_DE "de"        // German
+#define LANG_DV "dv"        // Divehi
+#define LANG_DZ "dz"        // Dzongkha
+#define LANG_EE "ee"        // Ewe
+#define LANG_EL "el"        // Greek
+#define LANG_EN "en"        // English
+#define LANG_EN_GB "en_GB"  // English (United Kingdom)
+#define LANG_EO "eo"        // Esperanto
+#define LANG_ES "es"        // Spanish
+#define LANG_ET "et"        // Estonian
+#define LANG_EU "eu"        // Basque
+#define LANG_FA "fa"        // Persian
+#define LANG_FF "ff"        // Fulah
+#define LANG_FI "fi"        // Finnish
+#define LANG_FJ "fj"        // Fijian
+#define LANG_FO "fo"        // Faroese
+#define LANG_FR "fr"        // French
+#define LANG_FY "fy"        // Western Frisian
+#define LANG_GA "ga"        // Irish
+#define LANG_GD "gd"        // Scottish Gaelic
+#define LANG_GL "gl"        // Galician
+#define LANG_GN "gn"        // Guaraní
+#define LANG_GU "gu"        // Gujarati
+#define LANG_GV "gv"        // Manx
+#define LANG_HA "ha"        // Hausa
+#define LANG_HE "he"        // Hebrew
+#define LANG_HI "hi"        // Hindi
+#define LANG_HO "ho"        // Hiri Motu
+#define LANG_HR "hr"        // Croatian
+#define LANG_HT "ht"        // Haitian Creole
+#define LANG_HU "hu"        // Hungarian
+#define LANG_HY "hy"        // Armenian
+#define LANG_HZ "hz"        // Herero
+#define LANG_IA "ia"        // International Auxiliary Language Association
+#define LANG_ID "id"        // Indonesian
+#define LANG_IE "ie"        // Interlingue
+#define LANG_IG "ig"        // Igbo
+#define LANG_II "ii"        // Sichuan Yi
+#define LANG_IK "ik"        // Inupiaq
+#define LANG_IO "io"        // Ido
+#define LANG_IS "is"        // Icelandic
+#define LANG_IT "it"        // Italian
+#define LANG_IU "iu"        // Inuktitut
+#define LANG_JA "ja"        // Japanese
+#define LANG_JV "jv"        // Javanese
+#define LANG_KA "ka"        // Georgian
+#define LANG_KG "kg"        // Kongo
+#define LANG_KI "ki"        // Kikuyu
+#define LANG_KJ "kj"        // Kwanyama
+#define LANG_KK "kk"        // Kazakh
+#define LANG_KL "kl"        // Kalaallisut
+#define LANG_KM "km"        // Khmer
+#define LANG_KN "kn"        // Kannada
+#define LANG_KO_KR "ko_KR"  // Korean (South Korea)
+#define LANG_KO_KP "ko_KP"  // Korean (North Korea)
+#define LANG_KR "kr"        // Kanuri
+#define LANG_KS "ks"        // Kashmiri
+#define LANG_KU "ku"        // Kurdish
+#define LANG_KV "kv"        // Komi
+#define LANG_KW "kw"        // Cornish
+#define LANG_KY "ky"        // Kirghiz
+#define LANG_LA "la"        // Latin
+#define LANG_LB "lb"        // Luxembourgish
+#define LANG_LG "lg"        // Ganda
+#define LANG_LI "li"        // Limburgish
+#define LANG_LN "ln"        // Lingala
+#define LANG_LO "lo"        // Lao
+#define LANG_LT "lt"        // Lithuanian
+#define LANG_LU "lu"        // Luba-Katanga
+#define LANG_LV "lv"        // Latvian
+#define LANG_MG "mg"        // Malagasy
+#define LANG_MH "mh"        // Marshallese
+#define LANG_MI "mi"        // Māori
+#define LANG_MK "mk"        // Macedonian
+#define LANG_ML "ml"        // Malayalam
+#define LANG_MN "mn"        // Mongolian
+#define LANG_MR "mr"        // Marathi
+#define LANG_MS "ms"        // Malay
+#define LANG_MT "mt"        // Maltese
+#define LANG_MY "my"        // Burmese
+#define LANG_NA "na"        // Nauru
+#define LANG_NB "nb"        // Norwegian Bokmål
+#define LANG_ND "nd"        // North Ndebele
+#define LANG_NE "ne"        // Nepali
+#define LANG_NG "ng"        // Ndonga
+#define LANG_NL "nl"        // Dutch
+#define LANG_NN "nn"        // Norwegian Nynorsk
+#define LANG_NO "no"        // Norwegian
+#define LANG_NR "nr"        // South Ndebele
+#define LANG_NV "nv"        // Navajo
+#define LANG_NY "ny"        // Chichewa
+#define LANG_OC "oc"        // Occitan
+#define LANG_OJ "oj"        // Ojibwa
+#define LANG_OM "om"        // Oromo
+#define LANG_OR "or"        // Oriya
+#define LANG_OS "os"        // Ossetian
+#define LANG_PA "pa"        // Panjabi
+#define LANG_PI "pi"        // Pāli
+#define LANG_PL "pl"        // Polish
+#define LANG_PS "ps"        // Pashto
+#define LANG_PT "pt"        // Portuguese
+#define LANG_PT_BR "pt_BR"  // Portuguese (Brazil)
+#define LANG_QU "qu"        // Quechua
+#define LANG_RM "rm"        // Raeto-Romance
+#define LANG_RN "rn"        // Kirundi
+#define LANG_RO "ro"        // Romanian
+#define LANG_RU "ru"        // Russian
+#define LANG_RW "rw"        // Kinyarwanda
+#define LANG_SA "sa"        // Sanskrit
+#define LANG_SC "sc"        // Sardinian
+#define LANG_SD "sd"        // Sindhi
+#define LANG_SE "se"        // Northern Sami
+#define LANG_SG "sg"        // Sango
+#define LANG_SI "si"        // Sinhalese
+#define LANG_SK "sk"        // Slovak
+#define LANG_SL "sl"        // Slovenian
+#define LANG_SM "sm"        // Samoan
+#define LANG_SN "sn"        // Shona
+#define LANG_SO "so"        // Somali
+#define LANG_SQ "sq"        // Albanian
+#define LANG_SR "sr"        // Serbian
+#define LANG_SS "ss"        // Swati
+#define LANG_ST "st"        // Sotho
+#define LANG_SU "su"        // Sundanese
+#define LANG_SV "sv"        // Swedish
+#define LANG_SW "sw"        // Swahili
+#define LANG_TA "ta"        // Tamil
+#define LANG_TE "te"        // Telugu
+#define LANG_TG "tg"        // Tajik
+#define LANG_TH "th"        // Thai
+#define LANG_TI "ti"        // Tigrinya
+#define LANG_TK "tk"        // Turkmen
+#define LANG_TL "tl"        // Tagalog
+#define LANG_TN "tn"        // Tswana
+#define LANG_TO "to"        // Tonga
+#define LANG_TR "tr"        // Turkish
+#define LANG_TS "ts"        // Tsonga
+#define LANG_TT "tt"        // Tatar
+#define LANG_TW "tw"        // Twi
+#define LANG_TY "ty"        // Tahitian
+#define LANG_UG "ug"        // Uyghur
+#define LANG_UK "uk"        // Ukrainian
+#define LANG_UR "ur"        // Urdu
+#define LANG_UZ "uz"        // Uzbek
+#define LANG_VE "ve"        // Venda
+#define LANG_VI "vi"        // Vietnamese
+#define LANG_VO "vo"        // Volapük
+#define LANG_WA "wa"        // Walloon
+#define LANG_WO "wo"        // Wolof
+#define LANG_XH "xh"        // Xhosa
+#define LANG_YI "yi"        // Yiddish
+#define LANG_YO "yo"        // Yoruba
+#define LANG_ZA "za"        // Zhuang
+#define LANG_ZH_CN "zh_CN"  // Simplified Chinese
+#define LANG_ZH_TW "zh_TW"  // Traditional Chinese
+#define LANG_ZU "zu"        // Zulu
