d849f4c187bdb28836c98e1c02a3819974ae9d4d
molingyu <z1522716486@hotmail.com>
2024-09-07 21:05:00 +0800

    ci: fix linux build script

--- build.sh
+++ build.sh
@@ -2,9 +2,9 @@
 
 rm -rf build
 mkdir build
-rmdir -rf src/gen
+rm -rf src/gen
 mkdir src/gen
-python .\gen\gen.py
+python ./gen/gen.py
 cd build
-cmake -i18n_format_DOCS=OFF ..
+cmake -DCMAKE_BUILD_TYPE="Debug" ..
 cmake --build .
